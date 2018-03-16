d5d2a1a13268168847177272faab584877805756
AtefBN <ben.nasr.atef@outlook.fr>
2018-03-16 10:23:55 +0100

    validation done, added utils functions

--- dev_form.js
+++ dev_form.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-  
 
 var issueModel = Backbone.Model.extend({
-        urlRoot: 'http://localhost:5001/1/issue/create',
+        urlRoot: 'https://test-errata-api.es-doc.org/1/issue/create',
         sync: function(method, model, options) {
           options.headers = {
             'Content-type': 'application/json',
-            'Connection': 'keep-alive',
             'Authorization': "Basic "+btoa(oauth_token+':'+username)
           };
           return Backbone.sync('create', model, options);
@@ -18,13 +16,13 @@ var issueModel = Backbone.Model.extend({
             if (!_.isEmpty(_.compact(this.errorModel.toJSON())))
                 return "Title must be longer than 5 characters.";
           }
-          else if (attrs.description.length < 10) {
+          if (attrs.description.length < 10) {
             this.errorModel.clear();
             this.errorModel.set({Description: "Description length requirement"});
             if (!_.isEmpty(_.compact(this.errorModel.toJSON())))
                 return "Description must be longer than 10 characters.";
           } 
-          else if (attrs.datasets.length <1){
+          if (attrs.datasets.length <1){
             this.errorModel.clear();
             this.errorModel.set({datasets: "Dataset list can't be empty"});
             if (!_.isEmpty(_.compact(this.errorModel.toJSON())))
@@ -32,30 +30,41 @@ var issueModel = Backbone.Model.extend({
           }
           
           //Test Materials and urls regex
-          else if (attrs.materials.length>0 || attrs.urls.length>0 || attrs.datasets.length>0){
+          if (attrs.materials.length>0 || attrs.urls.length>0 || attrs.datasets.length>0){
             if (attrs.datasets.length > 0){
-              console.log('datasets validation');
-              var datasetArray = attrs.datasets.split(',');
+              var datasetValid = true;
+              var datasetArray = String(attrs.datasets).split(',').map(item=>item.trim());
               var datasetPattern = /\w#\w|\w\.v\w/i;
-              for (var i in datasetArray){
-                var datasetValid = datasetPattern.test(datasetArray[i]);
-                console.log(datasetValid, datasetArray[i]);
+              if (!checkRegex(datasetArray, datasetPattern)){
+                datasetValid = false;
+                this.errorModel.clear();
+                this.errorModel.set({datasets: "Dataset format invalid"});
+                if (!_.isEmpty(_.compact(this.errorModel.toJSON())))
+                    return "Dataset format invalid needs to be dataset-id#version.";
               }
             }
             if (attrs.materials.length>0){
-              console.log('materials validation');
-              var materialsArray = attrs.materials.split(',');
+              var materialsValid = true;
+              var materialsArray = String(attrs.materials).split(',').map(item=>item.trim());
               var materialsPattern = /([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i;
-              for (var i in materialsArray){
-                var materialsValid = materialsPattern.test(materialsArray[i]);
+              if (!checkRegex(materialsArray, materialsPattern)){
+                urlsValid = false;
+                this.errorModel.clear();
+                this.errorModel.set({urls: "URLs item is invalid"});
+                if (!_.isEmpty(_.compact(this.errorModel.toJSON())))
+                  return "One of the urls provided has invalid format.";
               }  
             }
             if (attrs.urls.length>0){
-              console.log('urls validation');
-              var urlsArray = attrs.urls.split(',');
+              var urlsValid = true;
+              var urlsArray = String(attrs.urls).split(',').map(item=>item.trim());
               var urlsPattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-              for (var j in urlsArray){
-                var urlsValid = urlsPattern.test(urlsArray[j]);
+              if (!checkRegex(urlsArray, urlsPattern)){
+                urlsValid = false;
+                this.errorModel.clear();
+                this.errorModel.set({urls: "URLs item is invalid"});
+                if (!_.isEmpty(_.compact(this.errorModel.toJSON())))
+                  return "One of the urls provided has invalid format.";
               }
             }
         }
@@ -68,8 +77,8 @@ var issueData = new issueModel({
       title: 'Template title',
       status: "new", 
       description: 'Template description',
-      urls: "http://www.url1.com,http://www.url2.com", 
-      materials: "http://www.materials.com/1.jpeg,https://www.materials.com/2.png", 
+      urls: "http://www.google.com", 
+      materials: "https://test-errata.es-doc.org/media/img/materials/material-01.png,https://test-errata.es-doc.org/media/img/materials/material-02.png", 
       project: "cmip6",
       severity: "low", 
       datasets: "dataset-id1#version,dataset-id2#version"
@@ -81,6 +90,7 @@ var issueData = new issueModel({
 
 
 $("#jsonPreview").text(JSON.stringify(issueData.toJSON(), null, 2));
+
 
 myForm = new Backform.Form({
   el: "#form-errata",
@@ -165,6 +175,11 @@ myForm = new Backform.Form({
     control: "textarea",
     required: true,
     helpMessage: "Paste the affected dataset list seperate by a comma"
+  },
+  {
+    id: 'submitButton',
+    name: 'formSubmitButton',
+    control: 'button'
   }
   ]
 }).render();
@@ -172,41 +187,32 @@ myForm = new Backform.Form({
 // Creating an auxiliary issueData model to avoid triggering extra unneeded change event.
 issueData.on('change', function(){
       // jsonIssue = issueData.toJSON();
+      var submit = myForm.fields.get("submitButton");
       jsonIssue = issueData.clone();
-      jsonIssue.set('datasets', String(jsonIssue.get('datasets')).split(','));
-      jsonIssue.set('materials', String(jsonIssue.get('materials')).split(','));
-      jsonIssue.set('urls', String(jsonIssue.get('urls')).split(','));
+
+      jsonIssue.set('datasets', splitListAndClean(jsonIssue.get('datasets'), ','));
+      jsonIssue.set('materials', splitListAndClean(jsonIssue.get('materials'), ','));
+      jsonIssue.set('urls', splitListAndClean(jsonIssue.get('urls'), ','));
       if (myForm.model.isValid()){
         var additionalText = 'Validation successful';
         isValid = true;
+        submit.set({status:"success", message: "Success!"});
       }
       else {
         var additionalText = 'Validation failed: '+myForm.model.validationError;
+        submit.set({status:"error", message: myForm.model.validationError});
         isValid = false;
       }
       $("#jsonPreview").text(JSON.stringify(jsonIssue, null, 2) + '\n' + additionalText);
 });
 
-
-// $("#form-errata").on("change", function(e){
-//   var submit = myForm.fields.get("submitButton");
-//   if (myForm.model.isValid())
-//     submit.set({status:"success", message:"success"});
-//   else
-//     submit.set({status:"error", message:myForm.model.validationError});
-// });
-
-myForm.$el.on("submit",function(e){
-  console.log('down here');
-  // issueData.set('datasets' , jsonIssue.get('datasets'));
-  // issueData.set('materials', jsonIssue.get('materials'));
-  // issueData.set('urls', jsonIssue.get('urls'));
-  // issueData.on('invalid', function(model, error){
-  // })
-  // issueData.save();
-
+$('#form-errata .formSubmitButton').on("click",function(e){
+  issueData.set('datasets' , jsonIssue.get('datasets'));
+  issueData.set('materials', jsonIssue.get('materials'));
+  issueData.set('urls', jsonIssue.get('urls'));
+  issueData.on('invalid', function(model, error){
+  })
+  issueData.save();
   console.log('Successfully requested issue creation...');
 });
-// issueData.save();
-// console.log('success!');
 });
